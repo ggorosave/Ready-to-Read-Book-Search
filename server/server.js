6e6068854b69eef64de9332d06b5d2377452908d
ggorosave <103039442+ggorosave@users.noreply.github.com>
2023-02-15 16:16:19 -0700

    Made changes to required files for server and added todo comments

--- server/server.js
+++ server/server.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
+const { authMiddleware } = require('./utils/auth');
+
+// ADD typeDefs, resolvers 
+
 const db = require('./config/connection');
-const routes = require('./routes');
+// DELETE LATER
+// const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+// Add apollo server
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,8 +21,15 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+// DELETE LATER
+// app.use(routes);
 
+// sends html file
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'))
+})
+
+// Write start apollo server function and call it
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
