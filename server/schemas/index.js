cc54360d093b57263ebf84e70f326c0665c65905
ggorosave <103039442+ggorosave@users.noreply.github.com>
2023-02-16 15:49:50 -0700

    Added field list to the typeDefs file

---
+++ server/schemas/index.js
@@ -0,0 +1,1 @@
+// pull in typedefs and resolvers
---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,30 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+    type User {
+        _id: ID!
+        username: String!
+        email: String!
+        password: String!
+        savedBooks: [Book]
+    }
+
+    type Book {
+        authors: String
+        description: String!
+        bookId: String!
+        image: String
+        link: String
+        title: String!
+    }
+
+    type Query {
+        user(userId: ID!): User
+    }
+
+    type Mutation {
+        
+    }
+`;
+
+module.exports = typeDefs;
