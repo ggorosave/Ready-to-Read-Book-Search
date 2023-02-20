824717c4f539a99d57d5643eba26b06407d40c38
ggorosave <103039442+ggorosave@users.noreply.github.com>
2023-02-20 16:49:35 -0700

    Updated SignupForm component

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -60,9 +60,21 @@ const LoginForm = () => {
   return (
     <>
       <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
-        <Alert dismissible onClose={() => setShowAlert(false)} show={showAlert} variant='danger'>
-          Something went wrong with your login credentials!
-        </Alert>
+        {data ?
+          (
+            <p>Success!</p>
+          ) : (
+            <Alert dismissible onClose={() => setShowAlert(false)} show={showAlert} variant='danger'>
+              Something went wrong with your login credentials!
+            </Alert>
+          )}
+
+        {error && (
+          <div className="my-3 p-3 bg-danger text-white">
+            {error.message}
+          </div>
+        )}
+
         <Form.Group>
           <Form.Label htmlFor='email'>Email</Form.Label>
           <Form.Control
